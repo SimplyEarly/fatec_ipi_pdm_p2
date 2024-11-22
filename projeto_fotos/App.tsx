b07a9c22d5d59f370d9429f312a6d74e3d96ed1e
Joao <producoespjomal@gmail.com>
2024-11-21 22:55:05 -0300

    feat(p2-pdm): cria um botao e imagem de fundo

--- projeto_fotos/App.tsx
+++ projeto_fotos/App.tsx
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View, Pressable, Image } from 'react-native';
 
-export default function App() {
+const { height } = Dimensions.get('window');
+const altura_img = 200;
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./assets/Home.png')}
+        style={styles.imagemFundo}
+        resizeMode="cover"
+      />
+      <View style={styles.content}>
+        <Text style={styles.subtitulo}>
+          Aperte aqui para gerar 5 fotos de gatos!
+        </Text>
+        <Pressable style={styles.botao}>
+          <Text style={styles.textoBotao}>Gerar imagens😸</Text>
+        </Pressable>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  imagemFundo: {
+    position: 'absolute',
+    width: '100%',
+    height: height * 1.3,
+    top: 0,
+  },
+  content: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: altura_img,
+  },
+  subtitulo: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  botao: {
+    alignSelf: 'center',
+    width: '50%',
+    backgroundColor: 'darkorchid',
+    padding: 12,
+    borderRadius: 4,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  textoBotao: {
+    color: 'white',
+    textAlign: 'center',
   },
 });
+
+export default App;
